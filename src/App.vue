f4230a80027ba9eb7bfd262b50eec247b3644302
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-14 22:25:48 +0600

    complete

--- src/App.vue
+++ src/App.vue
@@ -4,64 +4,63 @@
 </template>
 
 <script>
-import Form from './components/A_from.vue';
-import Table from './components/A_Table.vue'
+import Form from "./components/A_from.vue";
+import Table from "./components/A_Table.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Form,
-    Table
+    Table,
   },
-  data(){
-    return{
-      details:JSON.parse(localStorage.getItem("details")) || [],
-      grentTotal: 0
-    }
+  data() {
+    return {
+      details: JSON.parse(localStorage.getItem("details")) || [],
+      grentTotal: 0,
+    };
   },
-  methods:{
-
-    submited(detailsItem){
-if(
-           detailsItem.date == "" ||
-           detailsItem.fullName == "" ||
-           detailsItem.address == "" ||
-           detailsItem.number == "" ||
-           detailsItem.email == "" ||
-           detailsItem.birth == "" ||
-           detailsItem.gender == "" ||
-           detailsItem.type == "" ||
-          detailsItem.amount == "" ||
-          detailsItem.description == "" 
-){
-alert ("Plase fill the from");
-return;
-}
-let data = {
-           date: detailsItem.date,
-           fullName: detailsItem.fullName,
-            address: detailsItem.address,
-            number: detailsItem.number,
-            email: detailsItem.email,
-            birth: detailsItem.birth,
-            gender: detailsItem.gender,
-            type: detailsItem.type,
-            amount: detailsItem.amount,
-            description: detailsItem.description
-};
-console.log(detailsItem, "not found");
-this.details.push(data);
- this.calculateTotal();
-this.saveToLocalStorage()
-console.log(this.details,"hi");
+  methods: {
+    submited(detailsItem) {
+      if (
+        detailsItem.date == "" ||
+        detailsItem.fullName == "" ||
+        detailsItem.address == "" ||
+        detailsItem.number == "" ||
+        detailsItem.email == "" ||
+        detailsItem.birth == "" ||
+        detailsItem.gender == "" ||
+        detailsItem.type == "" ||
+        detailsItem.amount == "" ||
+        detailsItem.description == ""
+      ) {
+        alert("Plase fill the from");
+        return;
+      }
+      let data = {
+        date: detailsItem.date,
+        fullName: detailsItem.fullName,
+        address: detailsItem.address,
+        number: detailsItem.number,
+        email: detailsItem.email,
+        birth: detailsItem.birth,
+        gender: detailsItem.gender,
+        type: detailsItem.type,
+        amount: detailsItem.amount,
+        description: detailsItem.description,
+      };
+      console.log(detailsItem, "not found");
+      this.details.push(data);
+      this.calculateTotal();
+      this.saveToLocalStorage();
+      console.log(this.details, "hi");
     },
 
-DeleteBtn(index){
-  this.details = this.details.filter((data, Toindex) => Toindex != index)
-  this.calculateTotal();
-  this.saveToLocalStorage()
-},
-calculateTotal() {
+    DeleteBtn(index) {
+      this.details = this.details.filter((data, Toindex) => Toindex != index);
+      this.calculateTotal();
+      this.saveToLocalStorage();
+    },
+    calculateTotal() {
       let sum = 0;
       for (let i = 0; i < this.details.length; i++) {
         sum += Number(this.details[i].amount);
@@ -69,17 +68,15 @@ calculateTotal() {
       this.grentTotal = sum;
       this.saveToLocalStorage();
     },
-saveToLocalStorage() {
+    saveToLocalStorage() {
       localStorage.setItem("details", JSON.stringify(this.details));
     },
   },
-  mounted(){
-  
-this.details = JSON.parse(localStorage.getItem("details"));
-this.calculateTotal();
-  }
-
-}
+  mounted() {
+    this.details = JSON.parse(localStorage.getItem("details"));
+    this.calculateTotal();
+  },
+};
 </script>
 
 <style lang="scss">
